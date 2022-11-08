734263841c8f8f029b9013bc4e187054834c9bae
Mog Rouhi <moghadase.roohi@gmail.com>
2022-11-08 11:02:33 +0000

    add env variable

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+require("dotenv").config();
+
+const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
@@ -10,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 });
@@ -32,6 +36,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("Server Running");
+server.listen(PORT, () => {
+  console.log("Server Running on port: ", PORT);
 });
